896d95510bbe1b9bbf891034f2fd05631587ab23
tidilia <tidiliaaa@gmail.com>
2026-06-11 18:12:36 +0500

    fix settings

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,5 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field, field_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
