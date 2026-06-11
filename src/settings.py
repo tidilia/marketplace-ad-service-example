233bc59baad4a10922c65ae051317e76f1f74e4b
tidilia <tidiliaaa@gmail.com>
2026-06-11 18:03:30 +0500

    trying to fix

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    database_url: str = "postgresql+asyncpg://postgres:postgres@localhost:5434/ads_db"
+    # database_url: str = "postgresql+asyncpg://postgres:postgres@localhost:5434/ads_db"
+    database_url: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/ads_db"
     jwt_secret: str = "change-me"
     jwt_algorithm: str = "HS256"
     kafka_bootstrap_servers: str = "localhost:9092"
